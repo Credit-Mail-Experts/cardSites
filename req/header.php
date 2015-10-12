181d1a6cd432dafaac8b54cd16391d29cfc0f8bd
Todd Urbanowicz <todd@creditmailexperts.com>
2015-10-12 10:36:38 -0400

    fix: added header to page

--- req/header.php
+++ req/header.php
@@ -1,5 +1,11 @@
 <header>
     <?php if (isset($employeeId)) {
         echo "<a href='logout.php' style='float: right;'>Logout</a>";
-    } ?>
+    }
+
+    if ($_SERVER['REQUEST_URI'] == '/call-center-login.php')  {
+        echo "<h1 style='text-align: center; padding-top: 10px;'>CME Call Center</h1>";
+    }
+
+    ?>
 </header>
